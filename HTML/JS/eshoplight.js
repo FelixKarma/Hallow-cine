7e195f7944709fd730780a6b1a64e421a400e5a7
JoeSupertramps <Delsalle.julie@gmail.com>
2023-10-02 14:16:52 +0200

    eshop

---
+++ HTML/JS/eshoplight.js
@@ -0,0 +1,19 @@
+const moins = document.getElementsByClassName ("moins");
+
+const plus = document.getElementsByClassName("plus");
+
+const nombre = document.getElementsByClassName("nombre");
+
+function ajout(num){
+    let nbr = parseInt(nombre[num-1].innerText);
+    nombre[num-1].innerText = nbr +1 
+};
+
+function supp(num){
+    if (nombre[num-1].innerText >0 ){
+    let nbr = parseInt(nombre[num-1].innerText);
+    nombre[num-1].innerText = nbr -1;
+}
+}
+const produits = document.getElementsByClassName ("btn-primary");
+
