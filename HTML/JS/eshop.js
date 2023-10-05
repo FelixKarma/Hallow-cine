fd9c7f74467da446cfe61b73619044638b557252
JoeSupertramps <Delsalle.julie@gmail.com>
2023-10-05 09:25:22 +0200

    shopfin

--- HTML/JS/eshop.js
+++ HTML/JS/eshop.js
@@ -1,5 +1,30 @@
 
 
+const moins = document.getElementsByClassName ("moins");
+
+const plus = document.getElementsByClassName("plus");
+
+const nombre = document.getElementsByClassName("nombre");
+
+function ajout(num){
+    let nbr = parseInt(nombre[num-1].innerText);
+    nombre[num-1].innerText = nbr +1 
+};
+
+function supp(num){
+    if (nombre[num-1].innerText >0 ){
+    let nbr = parseInt(nombre[num-1].innerText);
+    nombre[num-1].innerText = nbr -1;
+}
+}
+const produits = document.getElementsByClassName("btnAjoutPanier");
+
+function ajoutPanier(num) {
+    let nbr = parseInt(nombre[num-1].innerText);
+    if (nbr != 0) {
+        localStorage.setItem("produit"+(num),nbr);
+    }
+}
 // JS curseur souris
 
 const coords = { x: 0, y: 0 };
